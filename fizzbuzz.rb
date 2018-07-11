08a8da7c5e6090a8cb8ce0a6180c16a47aac9c7f
River Pease <riverpease15@gmail.com>
2018-07-11 15:14:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 def fizzbuzz(number)
-  if number % 
-  
+  if number % 3
+    puts "Fizz"
+  elsif number % 5
+    puts "Buzz"
+  elsif number % 5 && number % 3
+    puts "FizzBuzz"
+else
+  puts "nil"
+end
   
 end
