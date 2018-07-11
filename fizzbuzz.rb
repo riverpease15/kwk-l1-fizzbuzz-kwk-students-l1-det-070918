d61b2a20c8cb8cd966bd12f7657bf2ff15bd5f92
River Pease <riverpease15@gmail.com>
2018-07-11 15:20:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "Fizz"
+    puts "FizzBuzz"
   elsif number % 5 == 0 && number % 3 != 0
     puts "Buzz"
   elsif number % 3 == 0 
-    puts "FizzBuzz"
+    puts "Fizz"
 else
   puts "nil"
 end
